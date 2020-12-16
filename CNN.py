7fe6671e18735f62dea1aed8b4bdd32ec7fa507a
whtt <291191915@qq.com>
2020-12-16 10:07:38 +0800

    update a simple convolutional neural network

---
+++ CNN.py
@@ -0,0 +1,11 @@
+# /usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Intro  :
+@Project: PR_HW
+@File   : CNN.py
+@Author : whtt
+@Time   : 2020/12/16 10:06
+"""
+import torch
+import tortchvision
